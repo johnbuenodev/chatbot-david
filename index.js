0d269f59d417aa99324b2f3895686b06a8d79374
John_bueno <stevewolfjp@hotmail.com>
2020-04-16 14:36:16 -0300

    atualizando numero versao

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ var port = (process.env.PORT || 3000);
 
 const server = http.createServer((req,res)=>{
   res.writeHead(200, {'Content-Type':'application/json'});
-  res.write(JSON.stringify({name:'Davidbot',ver:'1.0.0'}));
+  res.write(JSON.stringify({name:'Davidbot',ver:'0.0.1'}));
   res.end();
 });
 
