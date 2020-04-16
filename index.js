3a63f614d74f7e9e63d0cb1ba6dabf05d8d4ae24
John_bueno <stevewolfjp@hotmail.com>
2020-04-16 14:17:18 -0300

    versao inicial chatbotdavid2

---
+++ index.js
@@ -0,0 +1,16 @@
+'use strict';
+
+var http = require('http');
+
+var port = (process.env.PORT || 3000);
+
+const server = http.createServer((req,res)=>{
+  res.writeHead(200, {'Content-Type':'application/json'});
+  res.write(JSON.stringify({name:'Davidbot',ver:'0.0.1'}));
+  res.end();
+});
+
+server.listen(port, ()=>{
+  console.log('Server on port');
+  console.log(port);
+})
